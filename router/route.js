74efcd2f7e10dc36546fcc9c2469e68d9cab9a49
d4cro <33950559+davecrowe55@users.noreply.github.com>
2021-01-06 12:30:17 +1300

    Added body parser, testing post route

--- router/route.js
+++ router/route.js
@@ -4,13 +4,33 @@ const app = express();
 const axios = require('axios');
 //Returns a list of orders ordered by the most recent
 
-//Test post route
-    app.post("/", async (req, res) => {
-      try {
-        return res.send("post route works");
-      } catch (error) {
-        console.error(error);
-      }
-    });
+//Need to extract payload from vend webhook
+app.post("/", async (req, res) => {
+  try { const { id } = req.body[`https://ebiketeam.vendhq.com/api/webhooks/`];
+  console.log(req.body);
+        // const { id } = req.body;
+    if (
+      id === undefined
+    ) {
+      return res.status(400).send("Bad request - missing parameters");
+    }
+    //create new inventory update
+    createInventoryUpdate(vendWebHook ,id);
+    res.status(201).json("OK - list was updated");
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).end();
+  }
+});
+
+//TEST POST ROUTE
+// app.post("/", async (req, res) => {
+//   try {
+//     return res.send("GREAT SUCCESS");
+//   } catch (error) {
+//     console.error(error);
+//   }
+// });
 
 module.exports = app;
