f059c2263cbeab9c9ef9ffb80ae35b032c6e35ae
d4cro <33950559+davecrowe55@users.noreply.github.com>
2021-01-06 11:02:06 +1300

    sql test data

--- db/alldata.sql
+++ db/alldata.sql
@@ -1,4 +1,13 @@
 CREATE TABLE payload
 (
-	id SERIAL PRIMARY KEY
+
+	id SERIAL PRIMARY KEY,
+	retailer_id int NOT NULL UNIQUE,
+	user_id int NOT NULL UNIQUE,
+	url "https://759827e72f05b9acbf7a0ec5acfd6b9c.m.pipedream.net",
+	type active,
+	
+
+  
+
 );
