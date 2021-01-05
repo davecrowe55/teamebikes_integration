70c3c5768710d9df9841f1d5a5d1d0eb596b2fe0
d4cro <33950559+davecrowe55@users.noreply.github.com>
2021-01-05 21:05:28 +1300

    edited route.js

--- router/route.js
+++ router/route.js
@@ -3,16 +3,7 @@ const express = require("express");
 const app = express();
 const axios = require('axios');
 //Returns a list of orders ordered by the most recent
-app.get("/contacts", async (req, res) => {
-    const contacts = `'https://api.hubapi.com/crm/v3/objects/contacts?limit=10&paginateAssociations=false&archived=false&hapikey=${process.env.HUBSPOT_API_KEY}'`
-      try {
-        const resp = await axios.get(contacts);
-        const data = resp.data;
-        res.json(data);
-      } catch (error) {
-        console.log(error);
-      }
-    });
+
 //Test post route
     app.post("/", async (req, res) => {
       try {
