9615ead91a0aa0a5f3d1fa5295cbe57c9fcdd70a
d4cro <33950559+davecrowe55@users.noreply.github.com>
2021-01-08 19:09:35 +1300

    heroku test

--- index.js
+++ index.js
@@ -316,7 +316,7 @@ app.get('/', async (req, res) => {
     const contact = await getContact(accessToken);
     const deal = await getDeal(accessToken);
     const db = await getHubdb(accessToken); //Auth issues doesn't allow public api display?
-    const pageInfor = await getPageInfo(accessToken); //Auth issues coming from pipedream, different auth, when coming straight from webhook should work?
+    const pageInfor = await getPageInfo(accessToken); //Auth issues coming from pipedream, different auth, when coming straight from webhook should work??
     res.write(`<h4>Access token: ${accessToken}</h4>`);
     displayContactName(res, contact);
     displayDeal(res, deal);
