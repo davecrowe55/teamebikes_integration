d73e08d360668a502818a6f674f58c3bd9521318
d4cro <33950559+davecrowe55@users.noreply.github.com>
2021-01-14 10:33:38 +1300

    Editing db.js

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
-const Pool = require("pg").Pool;
-
-const pool = new Pool({
-  // user: process.env.user,
-  // password: process.env.password,
-  // host: process.env.host,
-  // port: process.env.port,
-  // database: process.env.database
+const { Pool } = require("pg");
+var types = require('pg').types
+//pg won't cast by default as may lose precision.
+types.setTypeParser(1700, function(val) {
+  return parseFloat(val)
 });
+const pool = new Pool();
+module.exports = {
+  query: (text, params, callback) => {
+    return pool.query(text, params, callback);
+  },
+};
 
 module.exports = pool;
