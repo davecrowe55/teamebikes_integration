a913205e12c7a78584861478d3311639c9964f5e
d4cro <33950559+davecrowe55@users.noreply.github.com>
2021-01-14 16:47:13 +1300

    main js function test

--- index.js
+++ index.js
@@ -314,9 +314,9 @@ const displayPageInfo = (res, data) => {
 //Display data
 app.get('/', async (req, res) => {
   res.setHeader('Content-Type', 'text/html');
-  res.write(`<h2> DAVID'S BIGTEST testHubSpot OAuth 2.0 Quickstart App  (<button onClick="handleSignIn()">LOGIN </button>)login button test;`);
-  res.write("<input type='button' id='butTest' value='Login Database' onclick='location.href=\"route.js");
-  // res.render(<button onClick="handleSignIn()">LOGIN </button>);onClick="handleSignIn()">LOGIN>
+  res.write(`<h2> DAVID'S BIGTEST testHubSpot OAuth 2.0 Quickstart App  login button test;`);
+  res.write(`<button onClick="handleSignIn()">LOGIN </button>`);
+  // res.render(<button onClick="handleSignIn()">LOGIN </button>);
   if (isAuthorized(req.sessionID)) {
     const accessToken = await getAccessToken(req.sessionID);
     const contact = await getContact(accessToken);
--- main.js
+++ main.js
@@ -1,66 +1,66 @@
-// $(document).ready(function(){
-//     getposts();
-// })
+$(document).ready(function(){
+    getposts();
+})
 
-// function handleSignIn() {
-//     let provider = new firebase.auth.GoogleAuthProvider();
+function handleSignIn() {
+    let provider = new firebase.auth.GoogleAuthProvider();
 
-//     firebase.auth()
-//   .signInWithPopup(provider)
-//   .then((result) => {
-//     /** @type {firebase.auth.OAuthCredential} */
-//     let credential = result.credential;
+    firebase.auth()
+  .signInWithPopup(provider)
+  .then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+    let credential = result.credential;
 
     
-//     let token = credential.accessToken;
+    let token = credential.accessToken;
     
-//     let user = result.user;
-//     console.log(user.email);
-//   }).catch((error) => {
+    let user = result.user;
+    console.log(user.email);
+  }).catch((error) => {
     
-//     let errorCode = error.code;
-//     let errorMessage = error.message;
+    let errorCode = error.code;
+    let errorMessage = error.message;
     
-//     let email = error.email;
+    let email = error.email;
     
-//     let credential = error.credential;
+    let credential = error.credential;
     
-//   });
-// }
-// function addMessage(postTitle, postBody){
-//     let postData = {
-//         title: postTitle,
-//         body: postBody
-//     }
-//     var database = firebase.database().ref("posts");
+  });
+}
+function addMessage(postTitle, postBody){
+    let postData = {
+        title: postTitle,
+        body: postBody
+    }
+    var database = firebase.database().ref("posts");
 
-//     var newPostRef = database.push();
-//     newPostRef.set(postData, (error) => {
-//         if (error) {
-//         } else {
+    var newPostRef = database.push();
+    newPostRef.set(postData, (error) => {
+        if (error) {
+        } else {
           
-//           window.location.reload();
-//         }
-//       });
-//     }
+          window.location.reload();
+        }
+      });
+    }
 
-// function handleMessageFormSubmit(){
-//     let postTitle = $('#post-title').val();
-//     let postBody = $('#post-body').val();
-//     console.log(postTitle);
-//     addMessage(postTitle, postBody);
-// };
+function handleMessageFormSubmit(){
+    let postTitle = $('#post-title').val();
+    let postBody = $('#post-body').val();
+    console.log(postTitle);
+    addMessage(postTitle, postBody);
+};
 
-// function getposts(){
+function getposts(){
 
-//     return firebase.database().ref("posts").once('value').then(function(snapshot) {
-//         let posts = snapshot.val();
-//         console.log(posts);
+    return firebase.database().ref("posts").once('value').then(function(snapshot) {
+        let posts = snapshot.val();
+        console.log(posts);
        
 
-//         for(let postKey in posts) {
-//             let post = posts[postKey];
-//             $("#post-listing").append("<div>"+post.title+" - "+post.body+"</div>");
-//         }
-//       });    
-// };
+        for(let postKey in posts) {
+            let post = posts[postKey];
+            $("#post-listing").append("<div>"+post.title+" - "+post.body+"</div>");
+        }
+      });    
+};
